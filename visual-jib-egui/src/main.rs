44d706162f82bbf606a00be841bc5d7c894a3d34
Ian O'Rourke <cessna.ian@gmail.com>
2025-11-06 18:51:27 -0600

    UI Updates

--- visual-jib-egui/src/main.rs
+++ visual-jib-egui/src/main.rs
@@ -2,7 +2,8 @@ use std::{io::Cursor, sync::LazyLock, thread::JoinHandle};
 
 use cbuoy::CodeGenerationOptions;
 use eframe::egui::{
-    self, CentralPanel, Grid, IconData, ScrollArea, TextBuffer, TextEdit, Vec2, Visuals,
+    self, CentralPanel, Grid, IconData, Layout, ScrollArea, Slider, TextBuffer, TextEdit, Vec2,
+    Visuals,
 };
 use jib::cpu::RegisterManager;
 use jib_asm::InstructionList;
@@ -65,6 +66,7 @@ struct VisualJib {
     log_serial: String,
     log_text: String,
     text_serial_input: String,
+    text_debug_location: String,
     cpu_run_requested: bool,
     cpu_thread: Option<JoinHandle<()>>,
     tx_ui: std::sync::mpsc::Sender<UiToThread>,
@@ -73,6 +75,8 @@ struct VisualJib {
     registers: RegisterManager,
     memory_view: MemoryView,
     program_counter: ProgramCounterView,
+    current_cpu_speed: i32,
+    last_cpu_speed: i32,
 }
 
 impl VisualJib {
@@ -183,12 +187,15 @@ impl Default for VisualJib {
             log_serial: String::default(),
             log_text: String::default(),
             text_serial_input: String::default(),
+            text_debug_location: String::default(),
             tx_ui,
             rx_ui,
             tx_thread: tx_thread_local,
             registers: RegisterManager::default(),
             memory_view,
             program_counter: ProgramCounterView::default(),
+            current_cpu_speed: 0,
+            last_cpu_speed: 0,
         }
     }
 }
@@ -251,7 +258,7 @@ impl eframe::App for VisualJib {
         CentralPanel::default().show(ctx, |ui| {
             ui.horizontal(|ui| {
                 ui.vertical(|ui| {
-                    ui.heading("CPU");
+                    ui.heading("CPU Registers");
                     Grid::new("cpu_registers").show(ui, |ui| {
                         for (i, r) in self.registers.registers.iter().enumerate() {
                             if i > 0 && i % 2 == 0 {
@@ -262,46 +269,68 @@ impl eframe::App for VisualJib {
                         ui.end_row();
                     });
 
-                    if ui.button("Step").clicked() {
-                        self.tx_ui.send(UiToThread::CpuStep).unwrap();
-                    }
+                    ui.heading("Commands");
+                    Grid::new("cpu_commands").show(ui, |ui| {
+                        if ui.button("Step").clicked() {
+                            self.tx_ui.send(UiToThread::CpuStep).unwrap();
+                        }
 
-                    if ui.button("Start").clicked() {
-                        self.tx_ui.send(UiToThread::CpuStart).unwrap();
-                    }
+                        if ui.button("Start").clicked() {
+                            self.tx_ui.send(UiToThread::CpuStart).unwrap();
+                        }
 
-                    if ui.button("Stop").clicked() {
-                        self.tx_ui.send(UiToThread::CpuStop).unwrap();
-                    }
+                        if ui.button("Stop").clicked() {
+                            self.tx_ui.send(UiToThread::CpuStop).unwrap();
+                        }
 
-                    if ui.button("Reset").clicked() {
-                        self.tx_ui.send(UiToThread::CpuReset).unwrap();
-                    }
+                        ui.end_row();
 
-                    if ui.button("IRQ1").clicked() {
-                        self.tx_ui.send(UiToThread::CpuIrq(1)).unwrap();
+                        if ui.button("Reset").clicked() {
+                            self.tx_ui.send(UiToThread::CpuReset).unwrap();
+                        }
+
+                        if ui.button("IRQ1").clicked() {
+                            self.tx_ui.send(UiToThread::CpuIrq(1)).unwrap();
+                        }
+                    });
+
+                    ui.label("Speed Multiplier");
+                    ui.add(
+                        Slider::new(&mut self.current_cpu_speed, 1..=100)
+                            .step_by(5.0)
+                            .show_value(true),
+                    );
+
+                    if self.current_cpu_speed != self.last_cpu_speed {
+                        self.last_cpu_speed = self.current_cpu_speed;
+                        self.tx_ui
+                            .send(UiToThread::SetMultiplier(self.current_cpu_speed as f64))
+                            .unwrap();
                     }
                 });
 
                 ui.vertical(|ui| {
                     ui.heading("Memory");
-                    Grid::new("memory_view").show(ui, |ui| {
-                        for (i, v) in self.memory_view.values.iter().enumerate() {
-                            if i % MemoryView::COLUMNS == 0 {
-                                if i > 0 {
-                                    ui.end_row();
+                    Grid::new("memory_view")
+                        .spacing(Vec2 { x: 0.0, y: 0.0 })
+                        .show(ui, |ui| {
+                            for (i, v) in self.memory_view.values.iter().enumerate() {
+                                if i % MemoryView::COLUMNS == 0 {
+                                    if i > 0 {
+                                        ui.end_row();
+                                    }
+
+                                    ui.label(format!("L{:08x}", self.memory_view.base + i as u32));
                                 }
 
-                                ui.label(format!("L{:08x}", self.memory_view.base + i as u32));
+                                if let Some(b) = v {
+                                    ui.label(format!("{b:02x}"));
+                                } else {
+                                    ui.label("??");
+                                }
                             }
-
-                            if let Some(b) = v {
-                                ui.label(format!("{b:02x}"));
-                            } else {
-                                ui.label("??");
-                            }
-                        }
-                    });
+                        });
+                    ui.heading("Program Counter");
                     ui.label(format!(
                         "PC[0x{:08x}] = 0x{:08x}",
                         self.program_counter.pc, self.program_counter.val
@@ -311,16 +340,44 @@ impl eframe::App for VisualJib {
                         self.program_counter.get_instruction_string()
                     ));
 
+                    ui.heading("Breakpoint Location");
+                    if ui
+                        .text_edit_singleline(&mut self.text_debug_location)
+                        .lost_focus()
+                    {
+                        let (radix, s) =
+                            if let Some(rest) = self.text_debug_location.strip_prefix("0x") {
+                                (16, rest)
+                            } else {
+                                (10, self.text_debug_location.as_str())
+                            };
+
+                        let loc = if let Ok(v) = u32::from_str_radix(s, radix) {
+                            v
+                        } else {
+                            self.tx_thread
+                                .send(ThreadToUi::LogMessage(format!(
+                                    "unable to convert '{}' to program location",
+                                    self.text_debug_location
+                                )))
+                                .unwrap();
+                            0
+                        };
+
+                        self.tx_ui.send(UiToThread::SetBreakpoint(loc)).unwrap();
+                    }
+
+                    ui.heading("Serial Input");
                     TextEdit::singleline(&mut self.text_serial_input)
                         .return_key(None)
                         .show(ui);
-
                     if ui.button("Submit").clicked() {
                         self.tx_ui
                             .send(UiToThread::SerialInput(self.text_serial_input.take()))
                             .unwrap();
                     }
 
+                    ui.heading("Serial Log");
                     ScrollArea::vertical()
                         .id_salt("serial_log")
                         .stick_to_bottom(true)
@@ -331,9 +388,10 @@ impl eframe::App for VisualJib {
                         });
                 });
 
-                ui.vertical(|ui| {
+                ui.with_layout(Layout::top_down_justified(egui::Align::Center), |ui| {
+                    //ui.vertical(|ui| {
                     ui.heading("Code Entry");
-                    ui.vertical(|ui| {
+                    ui.horizontal(|ui| {
                         if ui.button("Assembly").clicked() {
                             self.code_selection = CodeSelection::Asm;
                         }
@@ -343,28 +401,32 @@ impl eframe::App for VisualJib {
                         }
                     });
 
-                    ScrollArea::both().show(ui, |ui| {
-                        let widget = if self.code_selection == CodeSelection::Asm {
-                            TextEdit::multiline(&mut self.code_asm)
-                        } else if self.code_selection == CodeSelection::CBuoy {
-                            TextEdit::multiline(&mut self.code_cbuoy)
-                        } else {
-                            panic!()
-                        }
-                        .code_editor();
+                    ScrollArea::both()
+                        .auto_shrink(false)
+                        .stick_to_bottom(true)
+                        .stick_to_right(true)
+                        .show(ui, |ui| {
+                            let widget = if self.code_selection == CodeSelection::Asm {
+                                TextEdit::multiline(&mut self.code_asm)
+                            } else if self.code_selection == CodeSelection::CBuoy {
+                                TextEdit::multiline(&mut self.code_cbuoy)
+                            } else {
+                                panic!()
+                            }
+                            .code_editor();
 
-                        if false {
-                            ui.add_sized(
-                                Vec2 {
-                                    x: 200.0,
-                                    y: ui.available_height(),
-                                },
-                                widget,
-                            );
-                        } else {
-                            ui.add(widget);
-                        }
-                    });
+                            if false {
+                                ui.add_sized(
+                                    Vec2 {
+                                        x: 200.0,
+                                        y: ui.available_height(),
+                                    },
+                                    widget,
+                                );
+                            } else {
+                                ui.add(widget);
+                            }
+                        });
 
                     if ui.button("Compile").clicked() {
                         if self.code_selection == CodeSelection::Asm {
